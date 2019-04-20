1dd27a68e01ace364ad536869c08a139adff6539
Phillyclause89 <45711864+Phillyclause89@users.noreply.github.com>
2019-04-20 06:27:08 -0700

    Add files via upload
    fixed an issue AND started to add comments

--- ANIMAL.py
+++ ANIMAL.py
@@ -2,16 +2,20 @@
 
 
 def numberchecker():
+    # First we prompt the user to enter a number or enter the quit command
     print("Enter Number or enter 'q' to quit:")
+    # Next we store our input as a variable called i
     i = input()
-    if i == "q":
+    # Then our outer if statement checks if one of 4 quit commands is entered.
+    if i == "q" or i == "Q" or i == "Quit" or i == "QUIT" or i == "quit":
+        # if the quit command is entered than we print good by and terminate the script by not calling the function again.
         print("Good bye!")
+    # We then nest the second if stament in a try statement just in case the input is not the quit command or convertible into an integer
     else:
         try:
+            # We try to convert our input into an integer
             n = int(i)
-            if i == "q":
-                print("Good bye!")
-            elif n % 32 == 0:
+            if n % 32 == 0:
                 print("ANIMAL")
                 numberchecker()
             elif n % 16 == 0:
